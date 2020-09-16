4d0b93e0c4242a8e52cc7d2b0bdce8d07261204f
haderlka <torobidum@gmail.com>
2020-09-16 20:01:15 +0200

    Added QTable example

--- TransactionBook/gui/TransactionWidget.py
+++ TransactionBook/gui/TransactionWidget.py
@@ -1,8 +1,20 @@
 import sys
 
-from PySide2.QtWidgets import QWidget, QMainWindow, QAction, QGridLayout, QTabWidget
+from PySide2.QtWidgets import QWidget, QTableWidget, QTableWidgetItem, QHBoxLayout
 
 
 class TransactionWidget(QWidget):
     def __init__(self, *args, **kwargs):
         super(TransactionWidget, self).__init__(*args, **kwargs)
+
+        layout = QHBoxLayout()
+        self.table = QTableWidget(100, 3)
+        self.columnLabels = ["Make","Model","Price"]
+        self.table.setHorizontalHeaderLabels(self.columnLabels)
+
+        test_table_content = QTableWidgetItem("Test data")
+        self.table.setItem(4, 1, test_table_content)
+
+        layout.addWidget(self.table)
+        self.setLayout(layout)
+
